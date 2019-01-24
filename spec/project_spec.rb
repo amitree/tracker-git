f1b298c4633c09a29f77dd35f29391082435c137
Ian Cole <ian@amitree.com>
2019-01-23 17:59:48 -0800

    [#163430981] Update to use tracker_api gem

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -6,9 +6,10 @@ describe Tracker::Project do
   let(:the_project) { double }
   let(:feature) { double }
   let(:bug) { double }
+  let(:tracker_client) { double }
 
-  describe "#initialize" do
-    it "initializes the project class" do
+  describe '#initialize' do
+    it 'initializes the project class' do
       project = Tracker::Project.new(tracker_token)
       project.should be
       project.tracker_token.should == tracker_token
@@ -19,41 +20,42 @@ describe Tracker::Project do
     let(:query) { double }
 
     before do
-      PivotalTracker::Project.should_receive(:all) { [the_project] }
-      the_project.should_receive(:stories) { query }
+      TrackerApi::Client.should_receive(:new).with(token: tracker_token).and_return(tracker_client)
+      tracker_client.should_receive(:projects).and_return([the_project])
     end
 
-    describe "#finished" do
+    describe '#finished' do
       before do
-        query.should_receive(:all).with(state: "finished", story_type: ['bug', 'feature']) { [feature, bug] }
+        the_project.should_receive(:stories).with(filter: 'state:finished type:bug,feature').and_return([feature, bug ])
       end
 
-      it "retrieves finished stories and bugs" do
+      it 'retrieves finished stories and bugs' do
         project = Tracker::Project.new(tracker_token)
         project.finished.should == [feature, bug]
       end
     end
 
-    describe "#finished_and_delivered" do
+    describe '#finished_and_delivered' do
       let(:query) { double }
 
       before do
-        query.should_receive(:all).with(state: ["finished","delivered"], story_type: ['bug', 'feature']) { [feature, bug] }
+        the_project.should_receive(:stories).with(filter: 'state:finished,delivered type:bug,feature').and_return([feature, bug ])
       end
 
-      it "retrieves finished and delivered stories and bugs" do
+      it 'retrieves finished and delivered stories and bugs' do
         project = Tracker::Project.new(tracker_token)
         project.finished_and_delivered.should == [feature, bug]
       end
     end
   end
 
-  describe "#deliver" do
+  describe '#deliver' do
     let(:project) { Tracker::Project.new(double) }
     let(:story) { double }
 
-    it "marks the story as delivered" do
-      story.should_receive(:update).with(current_state: "delivered")
+    it 'marks the story as delivered' do
+      story.should_receive(:current_state=).with('delivered')
+      story.should_receive(:save)
       project.deliver(story)
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
 
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = [:expect, :should]
+  end
+
+  config.expect_with(:rspec) { |c| c.syntax = :should }
+
   require File.dirname(__FILE__) + '/../lib/tracker-git'
-  require 'pivotal_tracker'
+  require 'tracker_api'
 end
